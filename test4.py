cc34daa14eacb361dbba4b7cf9b6217af1992366
wanjia0523 <64238465+wanjia0523@users.noreply.github.com>
2023-06-27 10:25:15 +0800

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,22 +1,53 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.types import StructType, StructField, FloatType, IntegerType
 
-spark = SparkSession.builder.appName("WineQualityPrediction").getOrCreate()
+# Create a SparkSession
+spark = SparkSession.builder.appName("WineDataExploration").getOrCreate()
 
-data = spark.read.csv("hdfs:///lab_test/wine.csv", header=True, inferSchema=True)
+# Define the schema for the wine data
+myschema = StructType([
+    StructField("fixed_acidity", FloatType(), True),
+    StructField("volatile_acidity", FloatType(), True),
+    StructField("citric_acid", FloatType(), True),
+    StructField("residual_sugar", FloatType(), True),
+    StructField("chlorides", FloatType(), True),
+    StructField("free_sulfur_dioxide", FloatType(), True),
+    StructField("total_sulfur_dioxide", FloatType(), True),
+    StructField("density", FloatType(), True),
+    StructField("pH", FloatType(), True),
+    StructField("sulphates", FloatType(), True),
+    StructField("alcohol", FloatType(), True),
+    StructField("quality", IntegerType(), True)
+])
 
-selected_columns = ["fixed acidity", "volatile acidity", "citric acid", "residual sugar",
-                    "chlorides", "free sulfur dioxide", "total sulfur dioxide", "density",
-                    "pH", "sulphates", "alcohol", "quality"]
+# Read the wine data from CSV
+wine = spark.read.format("csv") \
+    .schema(myschema) \
+    .option("header", True) \
+    .option("path", "hdfs:///lab_test/wine.csv") \
+    .load()
 
-data = data.select(selected_columns)
+# Print the schema of the wine DataFrame
+wine.printSchema()
 
-output_path = "hdfs:///lab_test/output"  # Specify the output directory path
+# Perform data exploration operations
+# Select wines with residual sugar less than 1.0 and order by quality
+filtered_wine = wine.filter(wine.residual_sugar < 1.0).orderBy("quality")
 
-data.write \
-    .format("com.mongodb.spark.sql.DefaultSource") \
-    .option("uri", "mongodb://127.0.0.1/wine_db.wine_collection") \
-    .mode("append") \
-    .save(output_path)  # Save the results to the specified output directory
+# Show the selected columns for the filtered wines
+filtered_wine.select("quality", "fixed_acidity", "volatile_acidity").show()
 
+# Calculate descriptive statistics for numeric columns
+numeric_columns = ["fixed_acidity", "volatile_acidity", "citric_acid", "residual_sugar",
+                   "chlorides", "free_sulfur_dioxide", "total_sulfur_dioxide", "density",
+                   "pH", "sulphates", "alcohol"]
+
+stats = wine.select(numeric_columns).describe()
+stats.show()
+
+# Save the exploration results as JSON
+output_path = "hdfs:///lab_test/exploration_output"
+filtered_wine.write.format("json").mode("overwrite").save(output_path)
+
+# Stop the SparkSession
 spark.stop()
