aee1f95d991b008bd74bf312617c9f1d9ecd258c
wanjia0523 <64238465+wanjia0523@users.noreply.github.com>
2023-06-27 11:59:32 +0800

    Update test6.py

--- test6.py
+++ test6.py
@@ -1,6 +1,5 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, FloatType, IntegerType
-pip install pandas
 # Create a SparkSession
 spark = SparkSession.builder.appName("CorrelationAnalysis").getOrCreate()
 
